629e121cc66e14c44e050abd2b4ceadc4a391ae1
konghiran.w <konghiran.w@linecorp.com>
2021-06-06 12:54:46 +0700

    update switch

--- 001_go_language_structure/6_switch_case.go
+++ 001_go_language_structure/6_switch_case.go
@@ -5,6 +5,19 @@ import "fmt"
 func main() {
 	x := 2
 	switchWithValue(x)
+
+	switchWithCondition(x)
+}
+
+func switchWithCondition(x int) {
+	switch { // จริงๆ ตรงนี้คือ switch true { } แต่ว่าเรา omit มัน
+	case x == 2:
+		fmt.Println("x is 2")
+	case x == 3:
+		fmt.Println("x is 3")
+	default:
+		fmt.Println("x is unknown")
+	}
 }
 
 func switchWithValue(x int) {
