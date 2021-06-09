5205343ad204cfa70e25deb8a4db2112f193a64b
konghiran.w <konghiran.w@linecorp.com>
2021-06-09 08:15:24 +0700

    add float

--- 002_data_types/2_floating_point.go
+++ 002_data_types/2_floating_point.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	// floating point follow standard IEEE754 http://mathcenter.oxford.emory.edu/site/cs170/ieee754/
+	fmt.Println("max float32:", math.MaxFloat32)
+	fmt.Println("max float64:", math.MaxFloat64)
+
+	var f float64
+	fmt.Println(f)
+	fmt.Println(f/0) // NaN (not a number) maybe 0.0000000000000000001/0
+	fmt.Println(1/f) // Inf (Infinity) maybe 1/0.00000000000000001
+
+	f = 3.2
+	fmt.Printf("f is %.2f\n",f)
 }
