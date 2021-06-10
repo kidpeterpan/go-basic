942dd9bc446bb9f73fee8bc750d4b5a598352cf4
konghiran.w <konghiran.w@linecorp.com>
2021-06-10 21:13:22 +0700

    add complex number

---
+++ 002_data_types/3_complex_number.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 3 + 4i อะไรประมาณนี้ (จำนวนเชิงซ้อน)
+	x := complex(7 , 3) // 7 + 3i
+	y := complex(1, 7)
+	z := x + y
+	fmt.Println(z) //8 + 10i
+}
