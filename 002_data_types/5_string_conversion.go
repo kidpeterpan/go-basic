49bfe2a2c78e33f4a4ea1e790027bdca02b962f8
konghiran.w <konghiran.w@linecorp.com>
2021-06-11 09:38:28 +0700

    update string conversion

--- 002_data_types/5_string_conversion.go
+++ 002_data_types/5_string_conversion.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	ex1 := string(rune(0x265e))
-	fmt.Println("EX1: Integer -> String") // จะได้ UTF-8
+	fmt.Println("EX1: Integer -> String") // จะได้ string UTF-8 represent
 	for i := 0; i < len(ex1); i++ {
 		fmt.Printf("%x", ex1[i])
 	}
 	fmt.Println("\xe2\x99\x9e")
 	fmt.Println("\xe2\x99\x9e" == ex1)
+
+	fmt.Println("EX2: []byte -> String")
+	ex2 := []byte{0xe2,0x99,0x9e}
+	ex2String := string(ex2)
+	fmt.Println(ex2String) // --> ♞
 }
