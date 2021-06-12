b15cbc83845ea107d06d1143ab6aa10b24143dfe
konghiran.w <konghiran.w@linecorp.com>
2021-06-12 07:26:51 +0700

    add basic slice

---
+++ 003_data_structure/2_slices.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fruits := [5]string{
+		"apple",
+		"banana",
+		"papaya",
+		"orange",
+		"mango",
+	}
+	sliceFruits := fruits[1:4]
+	fmt.Println(sliceFruits) // [orange mango]
+	fmt.Println(len(sliceFruits)) // 3
+	fmt.Println(cap(sliceFruits)) // 4
+
+	refSliceFruits := sliceFruits
+	refSliceFruits[0] = "super orange"
+	fmt.Println(refSliceFruits[0]) // "super orange"
+	fmt.Println(sliceFruits[0]) // "super orange" ถูกเปลี่ยนด้วยเพราะเป็น ref mem address ที่เดียวกัน
+}
