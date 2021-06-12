6eea02d38dbf018a5c127fde496546e8c02fa115
kidpeterpan <zealotkid711@gmail.com>
2021-06-12 16:54:26 +0700

    add draft map & struct

---
+++ 003_data_structure/5_maps.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+)
+// maps -> reference to hash table
+// key จะเป็นอะไรก็ได้ที่สามารถนำมา compare ได้ ( a == b )
+func main() {
+// ประกาศ map แบบ literal
+
+// ประกาศ map ด้วย make()
+
+// การเข้าถึง map element
+
+// test map is reference type
+
+// delete element from map
+
+// check ว่ามี key นี้ใน map ไหม
+
+// note: **map not garantee order**
+
+}
