c955afca6aaabaf399fe1b7f9fb400d2e20a6cb7
konghiran.w <konghiran.w@linecorp.com>
2021-06-12 20:06:43 +0700

    update embedded struct

--- 003_data_structure/8_struct_embeded.go
+++ 003_data_structure/8_struct_embeded.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 type People struct {
+	// note: หากต้องการ export field ให้ตั้งชื่อขึ้นต้นด้วยพิมพ์ใหญ่
 	name string
 	age int
 	Address // anonymouse field
@@ -14,12 +15,23 @@ type Address struct {
 
 func main(){
 	// Embedded struct คือ struct ที่มี member เป็น struct
-	// ใน go มี concept ของ anonymouse field
+	// ใน go มี concept ของ anonymous field
 	addr := Address{ zipcode: "10260" }
 	fmt.Printf("%+v\n",addr)
-	pp := People{
+	pp := People{ // because of we can not mix แบบระบุ field กับไม่ระบุ field ใน literal ได้
 		name: "Dang",
 		age: 10,
-	}	
+	}
+	pp.zipcode = addr.zipcode // สามารถเรียกใช้ pp.zipcode ได้เลย ไม่ต้อง pp.Address.zipcode
 	fmt.Printf("%+v", pp)
+
+	// ถ้าจะประกาศให้ assign zipcode ใน People ได้เลย
+	pp2 := People{
+		"Dang",
+		10,
+		Address{zipcode: "10260"},
+	}
+	fmt.Printf("\n%+v",pp2)
+	fmt.Printf("\n%s",pp2.zipcode) // 10260
+	fmt.Printf("\n%s",pp2.Address.zipcode) // หรือเราจะเรียก pp2.Address.zipcode ก็ทำได้ แต่มันยาว
 }
