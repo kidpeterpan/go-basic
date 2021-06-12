24b519cc76942ae207f0c81315c3de20e54e7d56
kidpeterpan <zealotkid711@gmail.com>
2021-06-12 19:30:56 +0700

    draft embedded struct

--- 003_data_structure/8_struct_embeded.go
+++ 003_data_structure/8_struct_embeded.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("embeded struct!")
+type People struct {
+	name string
+	age int
+	Address // anonymouse field
+}
+
+type Address struct {
+	zipcode string
+}
+
+func main(){
+	// Embedded struct คือ struct ที่มี member เป็น struct
+	// ใน go มี concept ของ anonymouse field
+	addr := Address{ zipcode: "10260" }
+	fmt.Printf("%+v\n",addr)
+	pp := People{
+		name: "Dang",
+		age: 10,
+	}	
+	fmt.Printf("%+v", pp)
 }
