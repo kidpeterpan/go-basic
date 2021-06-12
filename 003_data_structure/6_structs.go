5d9f13fd4d24320f5245460c2ea734a77663fd65
kidpeterpan <zealotkid711@gmail.com>
2021-06-12 17:19:43 +0700

    add basic struct

--- 003_data_structure/6_structs.go
+++ 003_data_structure/6_structs.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name string
+	age int
+}
+
 func main() {
-	fmt.Println("struct!")
+	// member ของ struct มี type ที่แตกต่างกันได้เช่น
+	deku := Person{ name: "deku", age: 14}	
+	fmt.Println(deku.name)
+	fmt.Println(deku.age)
+	fmt.Printf("\n")
+
+	// ประกาศ struct แบบ literal
+	uraraka := struct {
+		name string
+		age int 
+	}{
+		name: "uraraka",
+		age: 14,
+	}
+	fmt.Println(uraraka.name)
+	fmt.Println(uraraka.age)
+	fmt.Printf("\n")
 }
