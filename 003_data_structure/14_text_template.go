177b82699e2cbdc936889deecd3da1d1bd89461a
konghiran.w <konghiran.w@linecorp.com>
2021-06-13 07:05:06 +0700

    update text template

--- 003_data_structure/14_text_template.go
+++ 003_data_structure/14_text_template.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"text/template"
 )
 
@@ -11,12 +12,17 @@ type Item struct {
 	Count int
 }
 
+func upperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
 func main() {
 	items := []Item{
 		{Name: "bottle", Count: 2},
 		{Name: "mouse", Count: 3},
 	}
 	const itemDesTmp = `the item name {{ .Name }}, have {{ .Count }} {{ "\n" }}` // . is "Item", ส่วน \n มองเป็น string เลย require ""
+	// {{ }} สิ่งที่อยู่ด้านในเรียกว่า actions
 	tmp, err := template.New("ItemDescription").Parse(itemDesTmp)
 	// ใช้ template.Must ได้ถ้าขี้เกียจ handle err
 	if err != nil {
@@ -27,4 +33,16 @@ func main() {
 	_ = tmp.Execute(os.Stdout, items[0])
 	_ = tmp.Execute(os.Stdout, items[1])
 
+	// template 'RANGE' with arrays
+	fmt.Printf("\n")
+	fmt.Println("==== template 'RANGE' with arrays")
+	const itemDesTmpRange = `{{ range . }}the item name {{ .Name | upperCase }}, have {{ .Count }} {{ "\n" }}{{ end }}`
+	// note: ใช้ | เพื่อเรียก func ที่ register แล้ว
+	fm := make(template.FuncMap)
+	fm["upperCase"] = upperCase
+	tmpRange := template.Must(template.New("ItemDescriptionWithRange").
+		Funcs(fm). // register upperCase func
+		Parse(itemDesTmpRange))
+	fmt.Println(tmp.Name())
+	_ = tmpRange.Execute(os.Stdout, items)
 }
