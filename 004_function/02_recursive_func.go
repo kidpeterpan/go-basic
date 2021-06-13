09aa8f546fcf78b0d19415080cecebb462508e58
konghiran.w <konghiran.w@linecorp.com>
2021-06-13 11:04:55 +0700

    add factorial

--- 004_function/02_recursive_func.go
+++ 004_function/02_recursive_func.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	basicRecursiveCountdown(10) // 10 9 8 7 6 5 4 3 2 1
+	fmt.Println(factorial(5))   // 120
 }
 
 // note: recursive ประกอบด้วยสองส่วนหลักๆ
@@ -16,4 +17,11 @@ func basicRecursiveCountdown(x int) {
 	}
 	fmt.Print(x, " ")
 	basicRecursiveCountdown(x - 1)
+}
+
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+	return x * factorial(x-1) // (5*x) * (4*x) * (3*x) * (2*x) * (1*x) -> x = 1 -> 120
 }
