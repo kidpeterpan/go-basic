d6488d2de8b7f7808efbe7d50a9af06e09608633
konghiran.w <konghiran.w@linecorp.com>
2021-06-13 23:52:46 +0700

    add draft hof

---
+++ 004_function/07_higher_order_function.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// note: ***HOF คือ function ที่สามารถรับ function และ return function ได้***
+
+	// example with create fibonacci func
+
+	// add simple func for test fibonacci performance
+}
