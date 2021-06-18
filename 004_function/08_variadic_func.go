66fcb6fb952e3da4c4dd4baee36ec0b225effa6f
konghiran.w <konghiran.w@linecorp.com>
2021-06-18 21:47:24 +0700

    add variadic func

---
+++ 004_function/08_variadic_func.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+//variadicFunc use really well with option pattern in go
+func variadicFunc(characters ...string){
+	fmt.Println(reflect.TypeOf(characters)) // []string
+	fmt.Println(characters) // a b c
+}
+
+func main() {
+	variadicFunc("a","b","c")
+}
