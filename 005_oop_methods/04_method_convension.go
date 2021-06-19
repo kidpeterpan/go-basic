c92f425d8570964f4dbdfdb7edacddad49f84321
konghiran.w <konghiran.w@linecorp.com>
2021-06-19 12:47:51 +0700

    correct word convention

--- 005_oop_methods/04_method_convension.go
+++ 005_oop_methods/04_method_convension.go
@@ -4,20 +4,20 @@ import "fmt"
 
 // note: ถ้า receiver เป็น pointer ก็ให้เป็น pointer ให้หมด
 
-type Convension struct {
+type Convention struct {
 	rule string
 }
 
-func (c *Convension) SetRule(rule string) {
+func (c *Convention) SetRule(rule string) {
 	c.rule = rule
 }
 
-func (c *Convension) Rule() string {
+func (c *Convention) Rule() string {
 	return c.rule
 }
 
 func main() {
-	var conv Convension
+	var conv Convention
 	conv.SetRule("If apply pointer to receivers should apply to all of them")
 	fmt.Println(conv.Rule())
 }
